b8b6d1378bbb2820c4c949fbb2140db9368d42cd
AndreasKhmln <andreas.khmln@eduge.ch>
2022-03-17 07:51:25 +0100

    Images optimised and button colors upgraded

--- animation.js
+++ animation.js
@@ -1,27 +1,11 @@
-let value = "up";
-
-function SlideDown(el) {
-    console.log("SlideDown");
-    let elem = document.getElementById(el);
-    elem.style.transition = "all 5s ease-in-out";
-    elem.style.height = "100%";
-}
-
-function SlideUp(el) {
-    console.log("SlideUp");
-    let elem = document.getElementById(el);
-    elem.style.transition = "all 5s ease-in-out";
-    elem.style.height = "0px";
+function Init(el) {
+    document.getElementById(el).style.display = "none";
 }
 
 function Toggle(el) {
-    if (value == "up") {
-        console.log("up");
-        SlideDown(el);
-        value = "down";
-    } else if (value == "down") {
-        console.log("down");
-        SlideUp(el);
-        value = "up";
-    }
+    let elem = document.getElementById(el);
+    if (elem.style.display == "none")
+        elem.style.display = "block";
+    else
+        elem.style.display = "none";
 }
